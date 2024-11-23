96bec61cf94c928fe91fda540cddd9456f6a7a96
balasai45 <67778565+balasai45@users.noreply.github.com>
2024-11-23 20:45:41 +0530

    Delete dist directory

--- dist/notifications/dto/notifications.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class SendNotificationDto {
-    userId: string;
-    type: 'marketing' | 'newsletter' | 'updates';
-    channel: 'email' | 'sms' | 'push';
-    content: {
-        subject: string;
-        body: string;
-    };
-}
--- dist/user-preferences/interfaces/user-preferences.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=user-preferences.interface.js.map
